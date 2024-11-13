88234b3ee686b5d759ca3c3f9469a795e3ec3559
Ritesh Rathod <riteshrathod38@gmail.com>
2024-11-13 20:44:04 +0530

    background images added

--- src/modules/stake_unstake/StakeUnstake.tsx
+++ src/modules/stake_unstake/StakeUnstake.tsx
@@ -17,7 +17,7 @@ const StakeUnstake: React.FC = () => {
   };
 
   return (
-    <>
+    <div className="overflow-hidden">
       <div className="w-full h-full bg-gradient-to-b from-[#E9EFF4] to-[#FFFFFF]">
         <div className="flex flex-col justify-center items-center">
           <div className="flex space-x-[5.1vw] justify-center pt-[2.91vw]">
@@ -40,7 +40,7 @@ const StakeUnstake: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex flex-col w-fit ml-[6.3vw] rounded-[1.14vw] bg-[#EFF4F7] hover:bg-[#E5EBEF] ml-[1vw] pt-[0.67vw] pb-[0.83vw] px-[1.09vw] cursor-default absolute">
+        <div className="absolute z-10 flex flex-col w-fit ml-[6.3vw] rounded-[1.14vw] bg-[#EFF4F7] hover:bg-[#E5EBEF] ml-[1vw] pt-[0.67vw] pb-[0.83vw] px-[1.09vw] cursor-default absolute">
           <p className="text-[0.83vw] text-black text-center font-inter">
             Powered by
           </p>
@@ -56,9 +56,14 @@ const StakeUnstake: React.FC = () => {
           </h1>
         </div>
       </div>
+      <div className="relative">
+        <div className="absolute bg-[url('/src/assets/images/circle1.png')] bg-contain bg-center bg-no-repeat w-[29.98vw] h-[29.98vw] mt-[3.5vw] right-[-11vw] clip-path-[inset(0_9vw_0_0)]"></div>
+      </div>
       <Opportunities />
+      <div className="absolute bg-[url('/src/assets/images/circle2.png')] bg-contain bg-center bg-no-repeat w-[15.92vw] h-[15.92vw] mt-[21vw] right-[8.95vw]"></div>
+      <div className="absolute bg-[url('/src/assets/images/circle3.png')] bg-contain bg-center bg-no-repeat w-[29.98vw] h-[29.98vw] mt-[30vw] left-[-8vw]"></div>
       <FAQ />
-    </>
+    </div>
   );
 };
 
