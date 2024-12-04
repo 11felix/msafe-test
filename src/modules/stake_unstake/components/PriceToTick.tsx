8525b6fdee69ebaf585e9f367745b66a2495b3e1
Rushikesh Chavan <rushikeshchavan318@gmail.com>
2024-12-05 02:13:48 +0530

    price to tick component fixes

--- src/modules/stake_unstake/components/PriceToTick.tsx
+++ src/modules/stake_unstake/components/PriceToTick.tsx
@@ -6,6 +6,7 @@ interface Props{
 }
 function PriceToTick(props:Props) {
   const {} = props;
+
   return (
     <div className="w-[33.489vw] bg-white h-[16.822vw] rounded-[2.08vw] p-[2.08vw]">
       <div className="font-poppinsbold text-[1.56vw] mb-[1.52vw]">
@@ -17,7 +18,7 @@ function PriceToTick(props:Props) {
             <label className="text-[0.885vw] mb-[0.52vw] font-natosansbold">Enter Price</label>
             <input
               type="number"
-              className="w-[13.697vw] h-[2.08vw] border rounded-[0.364vw]"
+              className="w-[13.697vw] h-[2.08vw] border rounded-[0.364vw] no-spinner px-[0.45vw] font-poppinsbold text-[1.145vw] focus:outline-none focus:ring-0 leading-[1.145vw] "
             />
           </div>
             <button className="w-[13.697vw] h-[3.541vw] text-[1.25vw] text-center items-center rounded-[0.677vw] bg-[#3D5060] mt-[1.562vw] text-white font-poppinsbold">
@@ -29,8 +30,9 @@ function PriceToTick(props:Props) {
             <label className="text-[0.885vw] mb-[0.52vw] font-natosansbold">Enter Tick</label>
             <input
               type="number"
-              className="w-[13.697vw] h-[2.08vw] border rounded-[0.364vw]"
+              className="w-[13.697vw] h-[2.08vw] border rounded-[0.364vw] no-spinner px-[0.45vw] font-poppinsbold text-[1.145vw] focus:outline-none focus:ring-0 "
             />
+
           </div>
           <button className="w-[13.697vw] h-[3.541vw] text-[1.25vw] text-center items-center rounded-[0.677vw] bg-[#3D5060] mt-[1.562vw] text-white font-poppinsbold">
             Get Price
